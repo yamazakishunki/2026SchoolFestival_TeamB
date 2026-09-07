56b519752e449577e2754fe5d539c15f094e6338
Raden Sa'id Prana Putra <dm39life@gmail.com>
2026-09-08 08:38:54 +0900

    20260908_temp
    temp fix

--- School Festival 2026/Assets/Scripts/Menu/MainMenuManager.cs
+++ School Festival 2026/Assets/Scripts/Menu/MainMenuManager.cs
@@ -4,10 +4,15 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private string explanationSceneName = "Explanation";
+    [SerializeField] private string gameSceneName = "Farm";
 
-    public void OnPlayButton()
+    public void OnTutorialButton()
     {
         SceneManager.LoadScene(explanationSceneName);
+    }
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(gameSceneName);
     }
 
     public void OnExitButton()
